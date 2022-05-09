be8c4b2402eb03addd3bc558bdc3a44a1213688a
Junyoung <wnsdud12365@naver.com>
2022-05-09 15:30:54 +0900

    20220509

---
+++ SampleIntent/app/src/main/java/org/meoldae/sampleintent/MainActivity.java
@@ -0,0 +1,44 @@
+package org.meoldae.sampleintent;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_MENU = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_MENU);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_MENU){
+            Toast.makeText(getApplicationContext(), "Code : " + requestCode + " 결과 : " + resultCode, Toast.LENGTH_LONG).show();
+
+            if (resultCode == RESULT_OK){
+                String name = data.getStringExtra("name");
+                Toast.makeText(getApplicationContext(), "응답" + name, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
